3dbf6d2bd0f24302c6575aba53224eb630ffec15
Wun-Cian, Lan <ma930118@stust.edu.tw>
2021-07-09 13:53:47 +0800

    practice using GitHub

---
+++ Practice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice1
+//
+//  Created by 藍文謙 on 2021/7/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
